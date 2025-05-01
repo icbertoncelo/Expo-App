93b3d493d13abaa1331181c605cb247bbd1a3e9a
Ian Carlos Bertoncelo <icbertoncelo@MacBook-Pro-de-Ian.local>
2025-05-01 12:11:57 -0300

    feat: add image picker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,44 @@
 import { View, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
+import { useState } from "react";
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined
+  );
+
+  async function handlePickImageAsync() {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did nopt select any image");
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme="primary"/>
+        <Button
+          label="Choose a photo"
+          theme="primary"
+          onPress={handlePickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
     </View>
