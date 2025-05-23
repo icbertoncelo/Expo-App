ceebe4c68b6cd822b5355484516b8862ea9feee5
icbertoncelo <carlos.ian007@gmail.com>
2025-05-22 22:16:28 -0300

    feat: run expo prebuild and add native code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,15 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { LogLevel, OneSignal } from "react-native-onesignal";
 
 export default function RootLayout() {
+  useEffect(() => {
+    OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+    OneSignal.initialize(process.env.EXPO_PUBLIC_ONE_SIGNAL_APP_ID ?? "");
+    OneSignal.Notifications.requestPermission(true);
+  }, []);
+
   return (
     <>
       <Stack>
